7e81cdfeb3c0505cc512deac853f57de421cfd08
teach <regeditarivers@gmail.com>
2023-05-26 19:28:08 +0300

    first commit

---
+++ src/main/java/com/example/javacorecoursework2/JavaCoreCoursework2Application.java
@@ -0,0 +1,13 @@
+package com.example.javacorecoursework2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaCoreCoursework2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaCoreCoursework2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/javacorecoursework2/JavaCoreCoursework2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javacorecoursework2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaCoreCoursework2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
